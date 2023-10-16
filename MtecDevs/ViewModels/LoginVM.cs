01c3bb398a95f3eb82d5396321375f13a1a0143c
guilherme <guizinho.zaza14@gmail.com>
2023-10-16 09:38:10 -0300

    Criando configurações de login

---
+++ MtecDevs/ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace MtecDevs.ViewModels;
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+     public string Email { get; set; }
+    [Display(Name = "Senha de Acesso", Prompt = "Informe sua Senha")]
+    [Required(ErrorMessage = "Por favor, informe sua senha")]
+    public string Senha { get; set; }
+    [Display(Name = "Manter Conectado?")]
+    public bool Lembrar { get; set; } = false;
+    public string UrlRetorno { get; set; }
+}
