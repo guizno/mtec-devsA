dd6b228aedb57cfc3907b456feba14117348dc16
Guilherme <guizinho.zaza14@gmail.com>
2023-11-06 10:03:49 -0300

    Codificando login

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,25 +1,54 @@
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using MtecDevs.ViewModels;
 namespace MtecDevs.Controllers;
 
 public class AccountController : Controller
 {
     private readonly ILogger<AccountController> _logger;
-
-    public AccountController(ILogger<AccountController> logger)
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
     }
 
     [HttpGet]
-    public IActionResult Login()
+    public IActionResult Login(string returnUrl)
     {
+        LoginVM loginVM = new() {
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
         return View();
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
     {
-        return View("Error!");
+        if(ModelState.IsValid)
+        {
+            // Verifico o login
+            
+        }
+        //ModelState.AddModelError
+        return View(login);
+    }
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
